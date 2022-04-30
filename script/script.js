12b8b4b3c509a994985a019b94485192c046aec1
marzan131390 <marzandwmtec@gmail.com>
2022-05-01 03:08:23 +0600

    Responsive is done except toggle icon

---
+++ script/script.js
@@ -0,0 +1,7 @@
+//Display Nav Bar when toggle Button is clicked..
+let navItems = document.getElementById("nav-items");
+let toggleIcon = document.getElementById("toggle-icon");
+let navLink = document.getElementById
+toggleIcon.addEventListener("click", function() {
+    navItems.style.display = "block"; 
+})
